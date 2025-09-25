7bf112db74dcce933f27e967868c751aed93e910
Swathi Gopavarapu <swathigopavarapu1@gmail.com>
2025-09-25 10:53:50 +0530

    initial commit: event tracker

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import "./App.css"; // Make sure you include CSS below
+import "../styles/App.css"
 
 const localizer = momentLocalizer(moment);
 
